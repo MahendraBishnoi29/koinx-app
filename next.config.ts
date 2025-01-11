40ee35d2e59b77f4b2e759f160d76bc5031c98ad
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2025-01-11 13:27:32 +0530

    style: update Tailwind CSS configuration and global styles

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["coin-images.coingecko.com"],
+    domains: ["coin-images.coingecko.com", "s3-alpha-sig.figma.com"],
   },
 };
 
