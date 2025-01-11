c6187cf036da0a3662ea53d5806cc86329a45713
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2025-01-11 15:52:09 +0530

    fix: await params error for ssr in root page

--- src/app/[coinId]/page.tsx
+++ src/app/[coinId]/page.tsx
@@ -1,21 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import Loading from "@/components/ui/loading";
-import Navbar from "@/components/ui/navbar";
+import { Suspense } from "react";
+
+import { notFound } from "next/navigation";
+
 import CoinPage from "@/pages/home";
 import { CoinData } from "@/types/api";
-import { notFound } from "next/navigation";
-import { Suspense } from "react";
 import { getCoinData } from "../actions/coins";
+import Loading from "@/components/ui/loading";
+import Navbar from "@/components/ui/navbar";
 
-interface PageProps {
-  params: {
-    coinId: string;
-  };
+type PageProps = {
+  params: { coinId: string };
   searchParams: { [key: string]: string | string[] | undefined };
-}
+};
 
 export default async function Page({ params, searchParams }: PageProps) {
-  const data = await getCoinData(params.coinId);
+  const { coinId } = await params; // Await params
+
+  const data = await getCoinData(coinId);
 
   if ("error" in data) {
     notFound();
